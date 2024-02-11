e4a2f0db7bbad0a3212f9f3c2fe190240714f1e7
ASafin666 <ed.crowley@yandex.ru>
2024-02-11 15:34:13 +0300

    added another change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Heil Satan!");
+        System.out.println("Heil Satan!!!");
     }
 }
